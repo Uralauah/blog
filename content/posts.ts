e8dc137eec9f63771d41be9026f05aad06c08b7c
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-04-07 00:20:19 +0900

    sse 업로드

--- content/posts.ts
+++ content/posts.ts
@@ -1,5 +1,5 @@
 import post1 from '../notion-data/1c6a071d-4763-803a-8218-c59c905e3208.json'
-import post2 from '../notion-data/127ce18c-fd83-805c-bebd-d6772e18bf02.json'
+import post2 from '../notion-data/114a071d-4763-809a-8abf-d030eff337e2.json'
 
 const posts = [
   {
@@ -12,11 +12,11 @@ const posts = [
     category: "codingtest"
   },
   {
-    title: "Naver Search Bar UX: Analysis and Implementation Guide",
-    slug: "naver-search-bar-ux",
+    title: "SSE",
+    slug: "SSE",
     content: post2,
-    date: "2023-10-23",
-    description: "Explore the UX analysis of Naver's search bar and strategies for replicating its features. The post examines the UX triggers, selection of search results, and the refreshing of result lists. It also compares coding approaches for implementing search bar functionalities, aiming for a concise and effective execution.",
+    date: "2024-10-03",
+    description: "스프링부트의 SSE",
     image: undefined,
     category: "springboot"
   }
